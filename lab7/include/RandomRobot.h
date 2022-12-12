76bcda5a2160d3ccd17dd5d64070af1d3c3a3ada
Andrea Malvestio <andreamalv02@gmail.com>
2022-12-12 16:16:47 +0100

    added comments

--- lab7/include/RandomRobot.h
+++ lab7/include/RandomRobot.h
@@ -2,6 +2,7 @@
 #define RANDOMROBOT_H
 #include "Robot.h"
 
+//a Robot that moves chosing a random direction
 class RandomRobot : public Robot{
 public:
     RandomRobot(Tile mazePos) : Robot(mazePos){ srand(time(NULL)); }
